84acbfd271f0f462c34fc25485ae0f2747e6337a
edokadosh <edotiki@gmail.com>
2025-06-25 15:52:44 +0300

    fix proxy

--- router.py
+++ router.py
@@ -1,18 +1,31 @@
 from scapy.all import *
 
-SRC_IFACE = "enp0s8"
-DST_IFACE = "enp0s9"
+IN_IFACE = "enp0s8"
+OUT_IFACE = "enp0s9"
 
-SRC_LEG_IP = "192.168.56.101"
-DST_LEG_IP = "192.168.106.3"
+IN_LEG_IP = "192.168.56.101"
+OUT_LEG_IP = "192.168.106.3"
+
+OUT_LEG_MAC = "08:00:27:03:33:bc"
 
 def route(packet):
     print(f"Got packet from {packet[IP].src} to {packet[IP].dst}")
-    packet[IP].src = DST_LEG_IP
-    sendp(packet, iface=DST_IFACE)
+    new_packet = packet.copy()
+    new_packet[IP].src = OUT_LEG_IP
+    new_packet[Ether].src = OUT_LEG_MAC
+    new_packet[Ether].dst = "ff:ff:ff:ff:ff:ff"
+    new_packet[IP].ttl = packet[IP].ttl - 1
+
+    del new_packet[IP].chksum  # recalculate checksum
+    sendp(new_packet, iface=OUT_IFACE)
 
 def main():
-    sniff(iface=SRC_IFACE, prn= route, filter=f"ip and dst host {SRC_LEG_IP}")
+    sniff(iface=IN_IFACE, prn= route, filter=f"ip and dst host {IN_LEG_IP}")
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
