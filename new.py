3dd5f601f090f0f6eb7b28f8fe8de468775e0576
edokadosh <edotiki@gmail.com>
2025-06-30 09:57:42 +0300

    done with arp

--- new.py
+++ new.py
@@ -1,98 +1,65 @@
 from scapy.all import conf
-import struct
-from abc import ABC, abstractmethod
+from ModularPacketParser import ModularPacketParser
+from Ethernet import Ethernet, EthernetType
+from ARP import ARP, ARPOperation
+from LayerParser import Plaintext
 
-class LayerParser(ABC):
-    @abstractmethod
-    def parse(self, data):
-        pass
+# MY_MAC_ADDRESS = "08:00:27:90:bc:1b"
 
-    @abstractmethod
-    def encapsulate(self, payload, **kwargs):
-        pass
+def get_interface_info():
+    for index, i in enumerate(list(conf.ifaces.values())):
+        if i.mac:
+            print(f"{index}. Interface: {i.name}, MAC: {i.mac}")
 
-class Plaintext(LayerParser):
-    def __init__(self):
-        pass
+    interface = None
+    my_mac = None
+    my_ip = None
 
-    def parse(self, data):
-        return {
-            'payload': data
-        }
-
-    def encapsulate(self, payload):
-        return payload
-
-ETHERNET_HEADER_FORMAT = '!6s6sH'  # Destination MAC, Source MAC, EtherType
-ETHERTYPE_IPV4 = 0x0800
-
-class Ethernet(LayerParser):
-    def __init__(self, mac_address):
-        self.mac_address = mac_address.lower()
-
-    def parse(self, data):
-        dst_mac, src_mac, ethertype = struct.unpack(ETHERNET_HEADER_FORMAT, data[:14])
-        dst_mac = ':'.join(f'{b:02x}' for b in dst_mac).lower()
-        src_mac = ':'.join(f'{b:02x}' for b in src_mac).lower()
-        
-        if dst_mac == self.mac_address:
-            return {
-                'dst_mac': dst_mac,
-                'src_mac': src_mac,
-                'ethertype': ethertype,
-                'payload': data[14:]
-            }
-        return None
-    
-    def encapsulate(self, dst_mac, ethertype, payload):
-        dst_mac_bytes = bytes.fromhex(dst_mac.replace(':', ''))
-        src_mac_bytes = bytes.fromhex(self.mac_address.replace(':', ''))
-        ethertype_bytes = struct.pack('!H', ethertype)
-        return dst_mac_bytes + src_mac_bytes + ethertype_bytes + payload
-
-
-class ModularPacketParser:
-    def __init__(self, parsers: dict[str, LayerParser]):
-        self.parsers = parsers
-    
-    def parse(self, data):
-        parsed_packet = {}
-        for parser_name, parser in self.parsers:
-            parsed_layer = parser.parse(data)
-            if not parsed_layer:
-                return None
-            if parsed_layer:
-                parsed_packet[parser_name] = parsed_layer
-        
-        return parsed_packet
-
-    def encapsulate(self, **kwargs):
-        encapsulated_data = b''
-        for parser_name, parser in reversed(self.parsers.items()):
-            if parser_name in kwargs:
-                encapsulated_data = parser.encapsulate(**kwargs[parser_name], payload=encapsulated_data)
-        return encapsulated_data
-    
-
-MY_MAC_ADDRESS = "08:00:27:90:bc:1b"
+    while not my_mac:
+        interface_index = input("Enter the interface index: ")
+        if interface_index.isdigit():
+            interface_index = int(interface_index)
+        else:
+            print("Invalid input. Please enter a valid interface index.")
+            continue
+        if 0 <= interface_index < len(list(conf.ifaces.values())):
+            interface = list(conf.ifaces.keys())[interface_index]
+            my_mac = conf.ifaces[interface].mac
+            my_ip = conf.ifaces[interface].ip
+            print(f"Selected interface: {conf.ifaces[interface].name}")
+            print(f"Using MAC address: {my_mac}")
+            print(f"Using IP address: {my_ip}")
+        else:
+            print("Invalid interface. Please try again.")
+            
+    return interface, my_mac, my_ip
 
 def main():
+    interface, my_mac, my_ip = get_interface_info()
+
     parser = ModularPacketParser(parsers={
-        'ethernet': Ethernet(MY_MAC_ADDRESS),
-        'plaintext': Plaintext()
+        'ethernet': Ethernet(my_mac),
+        'arp': ARP(),
     })
 
     packet = parser.encapsulate(
         ethernet={
             'dst_mac': 'ff:ff:ff:ff:ff:ff',
-            'ethertype': ETHERTYPE_IPV4,
+            'ethertype': EthernetType.ARP.value,
         },
-        plaintext={
-            'payload': b'Hello, World!'
+        arp={
+            'sender_hardware': my_mac,
+            'sender_protocol': my_ip,
+            'target_hardware': 'ff:ff:ff:ff:ff:ff',
+            'target_protocol': '1.1.1.1',
+            'operation': ARPOperation.REQUEST.value
         }
     )
 
-    iface = "enp0s3"
+    iface = interface
     sock = conf.L2socket(iface=iface, promisc=True)
     sock.send(packet) # Send data
     recv = sock.recv_raw() # Receive data
+
+if __name__ == "__main__":
+    main()
