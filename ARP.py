0e6d354522f0133e91a2d9c4c2d1653653a94c39
edokadosh <edotiki@gmail.com>
2025-06-30 14:12:49 +0300

    done ping

--- ARP.py
+++ ARP.py
@@ -1,7 +1,7 @@
 import struct
 from LayerParser import LayerParser
 from enum import Enum
-
+from utils import mac_to_bytes, bytes_to_mac, ip_to_bytes, bytes_to_ip
 
 ARP_HEADER_FORMAT = '!HHBBH'  # Hardware type, Protocol type, Hardware size, Protocol size, Operation
 
@@ -27,10 +27,11 @@ class ARP(LayerParser):
     def __init__(self):
         pass
 
-    def parse(self, data):
+    def recv(self, data, raw_socket):
         arp_header_size = struct.calcsize(ARP_HEADER_FORMAT)
         if len(data) < arp_header_size:
-            return None
+            raise ValueError("Invalid ARP packet length")
+        
         hardware_type, protocol_type, hardware_size, protocol_size, operation = struct.unpack(ARP_HEADER_FORMAT, data[:arp_header_size])
         
         sender_hardware, sender_protocol, target_hardware, target_protocol = struct.unpack(
@@ -39,12 +40,12 @@ class ARP(LayerParser):
         )
 
         if hardware_type == HardwareType.ETHERNET.value:
-            sender_hardware = sender_hardware.hex().lower().replace(' ', ':')
-            target_hardware = target_hardware.hex().lower().replace(' ', ':')
+            sender_hardware = bytes_to_mac(sender_hardware)
+            target_hardware = bytes_to_mac(target_hardware)
         
         if protocol_type == ProtocolType.IPV4.value:
-            sender_protocol = '.'.join(str(b) for b in sender_protocol)
-            target_protocol = '.'.join(str(b) for b in target_protocol)
+            sender_protocol =  bytes_to_ip(sender_protocol)
+            target_protocol = bytes_to_ip(target_protocol)
     
         return {
             'hardware_type': hardware_type,
@@ -52,11 +53,11 @@ class ARP(LayerParser):
             'hardware_size': hardware_size,
             'protocol_size': protocol_size,
             'operation': operation,
-            'sender_hardware': sender_hardware.hex(),
-            'sender_protocol': sender_protocol.hex(),
-            'target_hardware': target_hardware.hex(),
-            'target_protocol': target_protocol.hex()
-        }
+            'sender_hardware': sender_hardware,
+            'sender_protocol': sender_protocol,
+            'target_hardware': target_hardware,
+            'target_protocol': target_protocol
+        }, None
 
     def encapsulate(self, sender_hardware, sender_protocol, target_hardware, target_protocol, operation):
         hardware_type = HardwareType.ETHERNET.value
@@ -74,10 +75,10 @@ class ARP(LayerParser):
             operation
         )                   
 
-        sender_hardware_bytes = bytes.fromhex(sender_hardware.replace(':', ''))
-        sender_protocol_bytes = bytes(map(int, sender_protocol.split('.')))
-        target_hardware_bytes = bytes.fromhex(target_hardware.replace(':', ''))
-        target_protocol_bytes = bytes(map(int, target_protocol.split('.')))
+        sender_hardware_bytes = mac_to_bytes(sender_hardware)
+        sender_protocol_bytes = ip_to_bytes(sender_protocol)
+        target_hardware_bytes = mac_to_bytes(target_hardware)
+        target_protocol_bytes = ip_to_bytes(target_protocol)
         arp_payload = struct.pack(
             f'!{hardware_size}s{protocol_size}s{hardware_size}s{protocol_size}s',
             sender_hardware_bytes,
